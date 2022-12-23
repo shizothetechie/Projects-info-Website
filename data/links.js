453fc6b59c8a45e9196aee4c54820a807ceefb3b
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2022-12-24 02:11:46 +0530

    Add files via upload

---
+++ data/links.js
@@ -0,0 +1,46 @@
+const links = [
+    {
+        id: 1,
+        name: "Website",
+        about: "Look at my work!",
+        url: "https://ssahibsingh.github.io",
+        img: require('../public/assets/website.webp')
+    },
+    {
+        id: 2,
+        name: "Github",
+        about: "@ssahibsingh | 🏡 to all my projects",
+        url: "https://github.com/ssahibsingh",
+        img: require('../public/assets/github.webp')
+    },
+    {
+        id: 3,
+        name: "LinkedIn",
+        about: "@ssahibsingh | Know more about my profession",
+        url: "https://www.linkedin.com/in/ssahibsingh/",
+        img: require('../public/assets/linkedin.webp')
+    },
+    {
+        id: 4,
+        name: "Twitter",
+        about: "@ssahibsingh_ | 😜",
+        url: "https://twitter.com/ssahibsingh_",
+        img: require('../public/assets/twitter.webp')
+    },
+    {
+        id: 5,
+        name: "Instagram",
+        about: "@ssahibsingh_ | Shots of my Life",
+        url: "https://www.instagram.com/ssahibsingh_/",
+        img: require('../public/assets/instagram.webp')
+    },
+    {
+        id: 6,
+        name: "Telegram",
+        about: "@cexnone | Chat with me instantly",
+        url: "https://t.me/cexnone",
+        img: require('../public/assets/telegram.webp')
+    },
+];
+
+export default links;
