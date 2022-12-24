0604db5f4dd3af88b5af5adab799f66486c61bb9
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2022-12-24 08:13:05 +0530

    Update links.js

--- data/links.js
+++ data/links.js
@@ -1,3 +1,4 @@
+
 const links = [
     {
         id: 1,
@@ -9,10 +10,38 @@ const links = [
     {
         id: 2,
         name: "Github",
-        about: "@shizkthetechie | 🏡 to all my projects",
-        url: "https://github.com/shizothetechie",
+        about: "@ssahibsingh | 🏡 to all my projects",
+        url: "https://github.com/ssahibsingh",
         img: require('../public/assets/github.webp')
     },
-   ;
+    {
+        id: 3,
+        name: "LinkedIn",
+        about: "@ssahibsingh | Know more about my profession",
+        url: "https://www.linkedin.com/in/ssahibsingh/",
+        img: require('../public/assets/linkedin.webp')
+    },
+    {
+        id: 4,
+        name: "Twitter",
+        about: "@ssahibsingh_ | 😜",
+        url: "https://twitter.com/ssahibsingh_",
+        img: require('../public/assets/twitter.webp')
+    },
+    {
+        id: 5,
+        name: "Instagram",
+        about: "@ssahibsingh_ | Shots of my Life",
+        url: "https://www.instagram.com/ssahibsingh_/",
+        img: require('../public/assets/instagram.webp')
+    },
+    {
+        id: 6,
+        name: "Telegram",
+        about: "@cexnone | Chat with me instantly",
+        url: "https://t.me/cexnone",
+        img: require('../public/assets/telegram.webp')
+    },
+];
 
 export default links;
