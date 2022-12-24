c5f27cfe035239cf8898b79577b6fd816f1d35c2
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2022-12-24 09:42:33 +0530

    Add files via upload

---
+++ data/links.js
@@ -0,0 +1,60 @@
+const links = [
+    {
+        id: 1,
+        name: "Website",
+        about: "Look at my Website!",
+        url: "https://shizo.shizo3d.repl.co",
+        img: require('../public/assets/website.webp')
+    },
+    {
+        id: 2,
+        name: "Website (siya)",
+        about: "Look at my Life Partner's Website!",
+        url: "https://siya.shizo3d.repl.co",
+        img: require('../public/assets/website.webp')
+    },
+    {
+        id: 3,
+        name: "Portfolio",
+        about: "CheckOut My Portfolio",
+        url: "https://portfolio.shizo3d.repl.co",
+        img: require('../public/assets/portfolio.png')
+    },
+    {
+        id: 4,
+        name: "BioLink",
+        about: "My BioLink Project | A Clone Of LinkTree with Style",
+        url: "https://linklist.shizo3d.repl.co",
+        img: require('../public/assets/bio-link.png')
+    },
+    {
+        id: 5,
+        name: "Love Calculator",
+        about: "Calculate Your Love | Love Calculator for couple",
+        url: "https://shizo-love-calculator.cyclic.app",
+        img: require('../public/assets/love-calculator.png')
+    },
+    {
+        id: 6,
+        name: "TE-WhatsApp",
+        about: "Discontinued | Modded WhatsApp with Immune",
+        url: "https://www.instagram.com/shizo_the_techie/",
+        img: require('../public/assets/tewhatsapp.png')
+    },
+    {
+        id: 7,
+        name: "SANG-MD",
+        about: "AI WhatsApp Robot | WhatsApp Robot with RPG Games & Groups Features",
+        url: "https://wa.me/message/6NUZF54RZPF7K1",
+        img: require('../public/assets/sang.png')
+    },
+    {
+        id: 8,
+        name: "MD-Session Generator",
+        about: "Multi-device Session Code generator using adhirajsing baileys",
+        url: "https://GitHub.com/shizothetechie/md-login",
+        img: require('../public/assets/session.png')
+    },
+];
+
+export default links;
